a8fa34744d005403aa7538526529f05c2c3defd9
thanexor <thanexor@gmail.com>
2015-04-06 22:34:01 -0500

    Players are assigned random team, and that team data is stored in localstorage.

--- js/Draft.js
+++ js/Draft.js
@@ -1,3 +1,17 @@
+var Storage = {
+    teamStorageKey: function (team) {
+        return 'draft-team' + team.id;
+    },
+
+    storeTeam: function (team) {
+        localStorage.setItem(this.teamStorageKey(team), ko.toJSON(team));
+    },
+
+    recallTeam: function (team) {
+        localStorage.setItem(this.teamStorageKey(team), ko.toJSON(team));
+    }
+};
+
 // MODEL FOR BANS
 // MODEL FOR BANS
 // MODEL FOR BANS
@@ -5,6 +19,7 @@ var DraftModel = function () {
     var rawRegData = {};
     var finishedLoading = ko.observable(false);
     var playerPool = ko.observableArray([]);
+    var teamModels = ko.observableArray([]);
 
     // var stubNames = [
     //     "Dad", "Mom", "Jason", "n0thing", "Boyer's penis",
@@ -28,7 +43,7 @@ var DraftModel = function () {
                 key: '1MK6tc6qXIhT7wTar0OFKPwyTN_ESgmnVQ7FKQ66HaoY',
                 callback: function(data, tabletop) {
                     rawRegData = data;
-                    fillPlayerPool(rawRegData);
+                    fillPlayersAndTeams(rawRegData);
                     finishedLoading(true);
                 },
                 simpleSheet: true 
@@ -39,12 +54,17 @@ var DraftModel = function () {
     fetchRegistrationData();
 
     var playerModel = function (playerData) {
+        var teamId = Math.floor(Math.random() * 8) + 1;
+
+        // console.log('id', teamId);
+
         return {
-            id: playerData[SHEET_KEY.time],
+            email: playerData[SHEET_KEY.email],
             alias: playerData[SHEET_KEY.alias],
             name: playerData[SHEET_KEY.name],
-            email: playerData[SHEET_KEY.email],
-            skill: playerData[SHEET_KEY.skill]
+            teamId: teamId,
+            skill: playerData[SHEET_KEY.skill],
+            regTime: playerData[SHEET_KEY.time],
         };
     };
 
@@ -56,7 +76,30 @@ var DraftModel = function () {
         }
     };
 
-    var fillPlayerPool = function (rawData) {
+    var movePlayerToTeam = function (player, teamId) {
+        var currentTeam = teamModels()[teamId-1];
+        currentTeam.players.push(player);
+        Storage.storeTeam(currentTeam);
+        playerPool.remove(player);
+    };
+
+    var assignPlayersToTeams = function () {
+        var i = playerPool().length;
+        var currentPlayer = null;
+        var currentPlayerTeamId = -1;
+
+        while (i--) {
+            currentPlayer = playerPool()[i];
+            currentPlayerTeamId = currentPlayer.teamId || -1;
+
+            // had team assignment already, move to team and remove from pool
+            if (currentPlayerTeamId !== -1) {
+                movePlayerToTeam(currentPlayer, currentPlayerTeamId);
+            }
+        }
+    };
+
+    var fillPlayersAndTeams = function (rawData) {
         var playerList = [];
 
         if (rawData) {
@@ -68,20 +111,21 @@ var DraftModel = function () {
         }
 
         playerPool(playerList);
+
+        var i = 8;
+
+        while (i--) {
+            teamModels.unshift(teamModel(i+1))
+        }
+
+        assignPlayersToTeams();
     };
 
     return {
         playerPool: playerPool,
         finishedLoading: finishedLoading,
-        team1: teamModel(1),
-        team2: teamModel(2),
-        team3: teamModel(3),
-        team4: teamModel(4),
-        team5: teamModel(5),
-        team6: teamModel(6),
-        team7: teamModel(7),
-        team8: teamModel(8)
-    }
+        teamModels: teamModels
+    };
 };
 
 var TheDraftModel = DraftModel();
@@ -92,34 +136,12 @@ var TheDraftModel = DraftModel();
 // VIEW MODEL FOR BANS
 // VIEW MODEL FOR BANS
 var DraftViewModel = function () {
-    var teamStorageKey = function (team) {
-        return 'draft-team' + team.id;
-    };
-
-    var storeTeam = function (team) {
-        localStorage.setItem(teamStorageKey(team), ko.toJSON(team));
-    };
-
-    var recallTeam = function (team) {
-        localStorage.setItem(teamStorageKey(team), ko.toJSON(team));
-    };
-
-
-    var allTeams = [
-        TheDraftModel.team1,
-        TheDraftModel.team2,
-        TheDraftModel.team3,
-        TheDraftModel.team4,
-        TheDraftModel.team5,
-        TheDraftModel.team6,
-        TheDraftModel.team7,
-        TheDraftModel.team8
-    ];
+    
 
     return {
         finishedLoading: TheDraftModel.finishedLoading,
         playerPool: TheDraftModel.playerPool,
-        teams: ko.observableArray(allTeams)
+        teams: TheDraftModel.teamModels
     };
 };
 
@@ -131,19 +153,14 @@ $(document).ready(function() {
 
     ko.applyBindings(Draft.vm);
 
-    // $.getJSON("https://spreadsheets.google.com/feeds/list/1MK6tc6qXIhT7wTar0OFKPwyTN_ESgmnVQ7FKQ66HaoY/ovvwzpc/public/values?alt=json", function(data) {
-    //     console.log(data.feed.entry);
-    // });
-
     var onReceive = function (e) {
         var $player = $(e.toElement);
         var $destinationList = $player.parent();
-        var teamIndex = parseInt($destinationList.data('teamid')) - 1
+        var teamIndex = parseInt($destinationList.data('teamid')) - 1;
 
         Draft.vm.teams()[teamIndex].players().push($player.text());
 
         storeTeam(Draft.vm.teams()[teamIndex]);
-        console.log(Draft.vm.teams()[teamIndex]);
         
     };
 
